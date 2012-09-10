72a85e1196aa2aab186e6e75f5db449f0fab587d
AbelCheng_cp <SND\AbelCheng_cp>
2012-09-10 02:20:05 +0000

    Add self-sensing triggers for derived instructions(ISTR) tables.

--- DataWashroom/OracleModel/6-InitData/01-WASH_ISTR_TYPE.sql
+++ DataWashroom/OracleModel/6-InitData/01-WASH_ISTR_TYPE.sql
@@ -1,25 +1,25 @@
 prompt Importing table WASH_ISTR_TYPE...
 set feedback off
 set define off
-insert into WASH_ISTR_TYPE (ISTR_TYPE, ISTR_BRIEF, DESCRIPTION_)
-values ('DELETE', 'Delete', 'Delete');
+insert into WASH_ISTR_TYPE (ISTR_TYPE, DERIVED_TABLE, TYPE_BRIEF, DESCRIPTION_)
+values ('DELETE', 'WASH_ISTR_DELETE', 'Delete', 'Delete');
 
-insert into WASH_ISTR_TYPE (ISTR_TYPE, ISTR_BRIEF, DESCRIPTION_)
-values ('COPY', 'Copy', 'Copy');
+insert into WASH_ISTR_TYPE (ISTR_TYPE, DERIVED_TABLE, TYPE_BRIEF, DESCRIPTION_)
+values ('COPY', 'WASH_ISTR_COPY', 'Copy', 'Copy matched columns (by column names)');
 
-insert into WASH_ISTR_TYPE (ISTR_TYPE, ISTR_BRIEF, DESCRIPTION_)
-values ('MERGE', 'Merge', 'Merge');
+insert into WASH_ISTR_TYPE (ISTR_TYPE, DERIVED_TABLE, TYPE_BRIEF, DESCRIPTION_)
+values ('MERGE', 'WASH_ISTR_MERGE', 'Merge', 'Merge(update and/or insert) data by matched columns');
 
-insert into WASH_ISTR_TYPE (ISTR_TYPE, ISTR_BRIEF, DESCRIPTION_)
-values ('CHK_UK', 'Check unique key', 'Check unique key, ');
+insert into WASH_ISTR_TYPE (ISTR_TYPE, DERIVED_TABLE, TYPE_BRIEF, DESCRIPTION_)
+values ('CHK_UK', 'WASH_ISTR_CHK_UK', 'Check unique key', 'Check supposed unique key');
 
-insert into WASH_ISTR_TYPE (ISTR_TYPE, ISTR_BRIEF, DESCRIPTION_)
-values ('RNK_DK', 'Sort duplicated key', 'Sort duplicated key - take top 1');
+insert into WASH_ISTR_TYPE (ISTR_TYPE, DERIVED_TABLE, TYPE_BRIEF, DESCRIPTION_)
+values ('RNK_DK', 'WASH_ISTR_RNK_DK', 'Sort duplicated key', 'Sort duplicated key - for taking top 1');
 
-insert into WASH_ISTR_TYPE (ISTR_TYPE, ISTR_BRIEF, DESCRIPTION_)
-values ('MUP_MK', 'Make up missing keys', null);
+insert into WASH_ISTR_TYPE (ISTR_TYPE, DERIVED_TABLE, TYPE_BRIEF, DESCRIPTION_)
+values ('MUP_MK', 'WASH_ISTR_MUP_MK', 'Make up missing keys', 'Make up missing keys in accordance with supposed foreign table');
 
-insert into WASH_ISTR_TYPE (ISTR_TYPE, ISTR_BRIEF, DESCRIPTION_)
-values ('MUP_NA', 'Make up a N/A key', null);
+insert into WASH_ISTR_TYPE (ISTR_TYPE, DERIVED_TABLE, TYPE_BRIEF, DESCRIPTION_)
+values ('MUP_NA', 'WASH_ISTR_MUP_NA', 'Make up a N/A key', 'Make up a N/A key (e.g. UNKNOWN, OTHER, DUMMY, -1, ...)');
 
 prompt Done.
